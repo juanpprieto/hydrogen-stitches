1ac513c6d3658329f9bbb9b57f6cbd50dbd5228e
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-06-26 18:35:05 -0700

    add globalStyles support

--- src/StitchesStyleTag.client.tsx
+++ src/StitchesStyleTag.client.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {getCssText} from '../stitches.config';
+import globalStyles from './globalStyles.client';
 
 export default function StitchesStyleTag() {
+  globalStyles();
+
   return (
     <style
       id="stitches"
